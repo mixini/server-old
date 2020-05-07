4dad250e617bde817d6ff4ed3a4cf907f7a2874e
Allen <fairingrey@gmail.com>
2020-05-06 20:36:24 -0500

    use tide and async-std instead

---
+++ server/src/main.rs
@@ -0,0 +1,16 @@
+use tide::log::LogMiddleware;
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    femme::start(log::Level::Info.to_level_filter()).unwrap();
+
+    let mut app = tide::new();
+
+    let log_middleware = LogMiddleware::new();
+    app.middleware(log_middleware);
+
+    app.at("/static").serve_dir("static/")?;
+
+    app.listen("127.0.0.1:3030").await?;
+    Ok(())
+}
