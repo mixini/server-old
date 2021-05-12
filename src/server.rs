3a77f0b9dd6fe553f4c1fbfef578882674e3ec28
Allen <fairingrey@gmail.com>
2021-05-12 09:04:01 -0700

    bind database conns to state

--- src/server.rs
+++ src/server.rs
@@ -1,24 +1,35 @@
-use oso::{Oso, OsoError};
+use anyhow::Result;
+use oso::Oso;
 use std::sync::Arc;
 use tide::log::{self, LogMiddleware};
 use tokio::sync::Mutex;
 
+use sqlx::PgPool;
+
 #[derive(Clone)]
 pub struct State {
     pub oso: Arc<Mutex<Oso>>,
+    pub db_pool: PgPool,
+    pub redis_client: redis::Client,
 }
 
 impl State {
     /// Attempt to create a new State instance
-    pub fn try_new() -> Result<State, OsoError> {
+    pub async fn try_new() -> Result<State> {
         let oso = Arc::new(Mutex::new(try_register_oso()?));
+        let db_pool = PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
+        let redis_client = redis::Client::open(std::env::var("REDIS_URL")?)?;
 
-        Ok(State { oso })
+        Ok(State {
+            oso,
+            db_pool,
+            redis_client,
+        })
     }
 }
 
 /// Attempt to create a new oso instance for managing authorization schemes.
-pub(crate) fn try_register_oso() -> Result<Oso, OsoError> {
+pub(crate) fn try_register_oso() -> Result<Oso> {
     let oso = Oso::new();
 
     // TODO: register polar files
@@ -27,10 +38,10 @@ pub(crate) fn try_register_oso() -> Result<Oso, OsoError> {
 }
 
 /// Run the server.
-pub async fn run() -> Result<(), tide::Error> {
+pub async fn run() -> Result<()> {
     log::start();
 
-    let mut app = tide::with_state(State::try_new()?);
+    let mut app = tide::with_state(State::try_new().await?);
 
     // middlewares
     app.with(LogMiddleware::new());
