794dd1059f69c996a5a4c8de2953cfa9aca63d50
Allen <fairingrey@gmail.com>
2022-02-09 15:18:54 -0800

    don't use test env in prod

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ pub(crate) mod utils;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Result<()>> {
-    dotenv::dotenv().ok();
+    if cfg!(debug_assertions) {
+        dotenv::dotenv().ok();
+    } else {
+        dotenv::from_filename("prod.env").ok();
+    }
 
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "mixini=debug")
