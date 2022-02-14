db11c801a360174435cdc4284819b7c7e7ebe8c2
Allen <fairingrey@gmail.com>
2022-02-14 09:59:26 -0800

    update deps also fix some logging keys

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<(), anyhow::Result<()>> {
     }
 
     if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "mixini=debug")
+        std::env::set_var("RUST_LOG", "mixini_server=debug,tower_http=debug")
     }
     tracing_subscriber::fmt::init();
 
