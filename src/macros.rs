8abb206ea2bd0a49d99990cba1ad47a2e90e9189
Allen <fairingrey@gmail.com>
2022-02-14 11:18:43 -0800

    fully qualify macro use

--- src/macros.rs
+++ src/macros.rs
@@ -4,33 +4,29 @@
 #[macro_export]
 macro_rules! impl_redis_rv {
     ($( $t:ty ),+) => {
-        use redis::{
-            ErrorKind as RedisErrorKind, FromRedisValue, RedisError, RedisResult, RedisWrite, ToRedisArgs,
-            Value as RedisValue,
-        };
         $(
-            impl ToRedisArgs for $t {
+            impl redis::ToRedisArgs for $t {
                 fn write_redis_args<W>(&self, out: &mut W)
                 where
-                    W: ?Sized + RedisWrite,
+                    W: ?Sized + redis::RedisWrite,
                 {
                     out.write_arg(&bincode::serialize(self).expect("Failed to bincode-serialize"));
                 }
             }
-            impl FromRedisValue for $t {
-                fn from_redis_value(v: &RedisValue) -> RedisResult<Self> {
-                    if let RedisValue::Data(data) = v {
+            impl redis::FromRedisValue for $t {
+                fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
+                    if let redis::Value::Data(data) = v {
                         let data_result = bincode::deserialize(data);
                         match data_result {
                             Ok(data) => Ok(data),
-                            Err(_) => Err(RedisError::from((
-                                RedisErrorKind::IoError,
+                            Err(_) => Err(redis::RedisError::from((
+                                redis::ErrorKind::IoError,
                                 "Response data not convertible",
                             ))),
                         }
                     } else {
-                        Err(RedisError::from((
-                            RedisErrorKind::TypeError,
+                        Err(redis::RedisError::from((
+                            redis::ErrorKind::TypeError,
                             "Response type not convertible",
                         )))
                     }
